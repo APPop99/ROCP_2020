960fbf38752697b999b82703641590fdd8b42d30
Paul Pop <APPop99@yahoo.com>
2020-11-06 05:07:12 +0200

    jdbc console bank app phase 0.5
    set-up of the unit test and logging service into the app

---
+++ jdbc_console_based_bank_project/src/main/java/app/bankingApp/service/impl/MyServiceImpl.java
@@ -0,0 +1,31 @@
+package app.bankingApp.service.impl;
+
+import app.bankingApp.service.MyService;
+
+public class MyServiceImpl implements MyService {
+
+	@Override
+	public boolean isValidPrimeNumber(int n) {
+		boolean b=false;
+		int c=0;
+		for (int i = 1; i <= n; i++) {
+			if(n%i==0) {
+				c++;
+			}
+		}
+		if(c==2) {
+			b=true;
+		}
+		return b;
+	}
+
+	@Override
+	public boolean isValidMobileNumber(String contact) {
+		if(contact!=null && contact.matches("\\+1-[0-9]{10}")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+}
